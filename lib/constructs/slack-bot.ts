e27f7b9f65a2c00a66b18924f7c92d91d995e3e5
mprey <masonprey7@gmail.com>
2025-04-07 20:31:05 -0400

    feat(*): add notifications for pipelines

---
+++ lib/constructs/slack-bot.ts
@@ -0,0 +1,29 @@
+import * as chatbot from 'aws-cdk-lib/aws-chatbot';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import { Construct } from "constructs";
+
+export interface SlackBotProps {
+  topics: sns.ITopic[];
+}
+
+export class SlackBot extends Construct {
+
+  constructor(scope: Construct, id: string, props: SlackBotProps) {
+    super(scope, id);
+
+    const role = new iam.Role(this, 'ChatBotRole', {
+      assumedBy: new iam.ServicePrincipal('chatbot.amazonaws.com'),
+      description: 'Role for AWS ChatBot',
+    });
+
+    new chatbot.SlackChannelConfiguration(this, 'SlackChannelConfiguration', {
+      slackChannelConfigurationName: "gnome-alerts-pipelines",
+      slackWorkspaceId: 'T08K71WNHSR',
+      slackChannelId: 'C08MU682NUQ',
+      notificationTopics: props.topics,
+      role,
+    });
+  }
+
+}
--- lib/gnome-shared-infra-stack.ts
+++ lib/gnome-shared-infra-stack.ts
@@ -1,16 +1,48 @@
 import * as cdk from 'aws-cdk-lib';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { PIPELINES } from './config';
+import { SlackBot } from './constructs/slack-bot';
 
 export class GnomeSharedInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const pipelineNotificationTopic = new sns.Topic(this, 'PipelineNotificationsTopic');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GnomeSharedInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    PIPELINES.forEach(pipelineName => {
+      // Pipeline-level events (Succeeded, Failed, Started)
+      new events.Rule(this, `${pipelineName}ExecutionRule`, {
+        eventPattern: {
+          source: ['aws.codepipeline'],
+          detailType: ['CodePipeline Pipeline Execution State Change'],
+          detail: {
+            pipeline: [pipelineName],
+            state: ['SUCCEEDED', 'FAILED', 'STARTED'],
+          },
+        },
+        targets: [new targets.SnsTopic(pipelineNotificationTopic)],
+      });
+    
+      // Action-level events (Manual Approval)
+      new events.Rule(this, `${pipelineName}ApprovalRule`, {
+        eventPattern: {
+          source: ['aws.codepipeline'],
+          detailType: ['CodePipeline Action Execution State Change'],
+          detail: {
+            pipeline: [pipelineName],
+            actionCategory: ['Approval'],
+            state: ['STARTED'],
+          },
+        },
+        targets: [new targets.SnsTopic(pipelineNotificationTopic)],
+      });
+    });
+
+    new SlackBot(this, 'SlackBot', {
+      topics: [pipelineNotificationTopic],
+    });
   }
 }
